5670d4b3b0d491d0c1800299b54618d1b2964162
임준식 <yjs3694@gmail.com>
2017-12-13 15:28:47 +0900

    ver04 추가

---
+++ 1.samplePrj/src/Book_vr04/BookApp.java
@@ -0,0 +1,111 @@
+package Book_vr04;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BookApp {
+
+	public static void main(String[] args) {
+		
+		Book[] data = {
+				new Book("Servlet",500),
+				new Book("자바 프로그래밍",900),
+				new Book("SQL",700)
+				
+		};
+		
+		BookMgr bm = null;
+		try {
+			bm = new BookMgr();
+		} 
+		catch (FileNotFoundException e) {
+			bm = new BookMgr(data);
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int cmd = 0;
+
+		
+		while (true) {
+
+			System.out.println("========================");
+			System.out.println("도서 정보 프로그램");
+			System.out.println("1. 도서 목록 보기");
+			System.out.println("2. 도서 등록");
+			System.out.println("3. 도서 삭제");
+			System.out.println("4. 도서 검색");
+			System.out.println("5. 저장");
+			
+			System.out.println("6. 프로그램 종료");
+			System.out.println("========================");
+			System.out.print("원하는 명령을 입력하세요.");
+
+			try {
+				cmd = Integer.parseInt(scanner.nextLine().trim());
+
+			} catch (Exception e) {
+				System.out.println("숫자로만 입력해주세요.");
+			} finally {
+			}
+
+			switch (cmd) {
+
+			case 0:
+				break;
+
+			case 1:
+				bm.printBookList();
+				break;
+
+			case 2:
+				System.out.print("등록할 도서 제목을 입력하세요.");
+				String title = scanner.nextLine();
+
+				System.out.print("등록할 도서 가격을 입력하세요.");
+				int price = scanner.nextInt();
+
+				scanner.nextLine(); // enter 라인을 소진 시켜주기 위해서 들어가야 함?
+
+				Book data1 = new Book(title, price);
+				bm.addBook(data1);
+				// bm.printBookList();
+				System.out.println("등록된 도서정보는 다음과 같습니다.");
+				bm.searchBookList(title);
+				break;
+
+			case 3:
+				System.out.println("삭제작업 수행.");
+				System.out.println("삭제할 도서 제목을 입력하세요.");
+				title = scanner.nextLine();
+				bm.delete(title);
+				break;
+
+			case 4:
+				System.out.println("도서 검색 기능을 수행합니다.");
+				System.out.println("검색할 도서 제목을 입력하세요");
+				title = scanner.nextLine();
+				bm.searchBookList(title);
+				break;
+
+			case 5 :
+				System.out.println("도서목록을 파일에 저장합니다.");
+				bm.filesave();
+				break;
+				
+			case 6:
+				System.out.println("프로그램을 종료합니다.");
+				System.exit(0); // 프로그램 종료
+				break;
+
+			default:
+				System.out.println("다시 선택해주세요.");
+				break;
+			}
+		}
+	}
+
+}
---
+++ 1.samplePrj/src/Book_vr04/BookMgr.java
@@ -0,0 +1,124 @@
+package Book_vr04;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class BookMgr {
+	private List<Book> bookList = new ArrayList<Book>();
+
+	FileBook file = new FileBook();
+	String fileName = "c:/file/book.data";
+
+	public BookMgr() throws Exception {
+
+		bookList = (List<Book>) file.read(fileName);
+
+		if (bookList == null || bookList.size() == 0) {
+
+			String fileName = "c:/file/sample1.txt";
+			Scanner s = new Scanner(new File(fileName));
+
+			while (s.hasNextLine()) {
+				String readLine = s.nextLine();
+				String[] data = readLine.split("/");
+				bookList.add(new Book(data[0], Integer.parseInt(data[1].trim())));
+			}
+		}
+		
+		Thread job = new AutoSave();
+		job.start();
+
+	}
+
+	public void filesave() {
+
+		file.save(bookList, fileName);
+
+	}
+
+	public BookMgr(Book[] data) {
+	
+		
+	
+	}
+
+	public void addBook(Book data) {
+		bookList.add(data);
+	}
+
+	public void printBookList() {
+		for (int i = 0; i < bookList.size(); i++) {
+			bookList.get(i).display();
+		}
+	}
+
+	public void printTitleList() {
+		for (int i = 0; i < bookList.size(); i++) {
+			System.out.println(bookList.get(i).getTitle());
+		}
+	}
+
+	// 검색 기능추가
+	public void searchBookList(String title) {
+		int c = 0;
+
+		for (Book data : bookList) {
+			if (data.getTitle().equals(title)) { // i번째 book리스트에서
+				data.display();
+				c++;
+			}
+			if (c == 0) { // 검색된 데이터가 없다는 뜼.
+				System.out.println("요청한 도서가 없습니다.");
+			}
+		}
+	}
+
+	// 삭제 기능 추가
+	// .....
+
+	public void delete(String title) {
+
+		Iterator<Book> it = bookList.iterator();
+		while (it.hasNext()) {
+			Book book = (Book) it.next();
+			if (book.getTitle().equals(title)) {
+				System.out.println("삭제 도서 정보 :" + book);
+				it.remove();
+
+			}
+
+		}
+
+	}
+
+	public void printTotalPrice() {
+		int total = 0;
+		for (int i = 0; i < bookList.size(); i++) {
+			total += bookList.get(i).getPrice();
+		}
+		System.out.println("전체 책 가격의 합 : " + total);
+	}
+
+	class AutoSave extends Thread {
+
+		@Override
+		public void run() {
+
+			while (true) {
+				try {
+					Thread.sleep(50000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				file.save(bookList, fileName);
+				System.out.println("AutoSave...");
+			}
+
+		}
+
+	}
+
+}
